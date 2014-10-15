6dc5057a5c603cfdb7e8dec8ec742a7a8d4f4d57
Chiru <Chiru@MININT-M2LELIK>
2014-10-14 23:14:14 -0400

    Finished the Dictionary.

--- Dictionary/src/Spellcheck/Tester.java
+++ Dictionary/src/Spellcheck/Tester.java
@@ -1,5 +1,14 @@
 package Spellcheck;
 
-public class Tester {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 
+public class Tester {
+public static void main(String[] args) throws FileNotFoundException{
+	SpellCheck spell=new SpellCheck(new File(args[0]));
+
+	
+	System.out.println(spell.checkSpelling("Hello i suchsasc at spewkwe"));
+	}
 }
